1d4bf6e678b7388b93d4cf1f64d1e83d249764a5
eminarican <emin.arican@outlook.com>
2021-07-28 23:02:22 +0300

    update main example

--- main.go
+++ main.go
@@ -9,22 +9,23 @@ import (
 
 func main() {
 	r := room.New()
+	l := r.Logger()
 	defer r.Shutdown()
 
 	r.OnPlayerJoin(func(p *room.Player) {
-		r.Logger().Info("A player joined to room!")
+		l.Info("A player joined to room!")
 	})
 
 	r.OnPlayerLeave(func(p *room.Player) {
-		r.Logger().Info("A player leaved from room!")
+		l.Info("A player leaved from room!")
 	})
 
 	r.OnPlayerChat(func(p *room.Player, msg string) (send bool) {
-		r.Logger().Infof("%v:%v", p.Name(), msg)
+		l.Infof("%v:%v", p.Name(), msg)
 		return true
 	})
 
-	r.Logger().Infof("Successfully started! Room link: %v", r.Link())
+	l.Infof("Successfully started! Room link: %v", r.Link())
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
