97011af3028a5561b366eeb02a17999138f84e85
eminarican <emin.arican@outlook.com>
2021-07-29 23:47:25 +0300

    update.

--- room/scheduler.go
+++ room/scheduler.go
@@ -6,19 +6,19 @@ import (
 
 type Scheduler struct{}
 
-func (Scheduler) Delayed(duration time.Duration, fun func()) {
+func (Scheduler) Delayed(delay time.Duration, fun func()) {
 	go func() {
-		ticker := time.NewTicker(duration)
+		ticker := time.NewTicker(delay)
 		<-ticker.C
 		ticker.Stop()
 		fun()
 	}()
 }
 
-func (Scheduler) Repeating(duration time.Duration, fun func()) func() {
+func (Scheduler) Repeating(period time.Duration, fun func()) func() {
 	cancel := make(chan bool)
 	go func() {
-		ticker := time.NewTicker(duration)
+		ticker := time.NewTicker(period)
 		for {
 			select {
 			case <-ticker.C:
