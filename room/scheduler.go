9cd269264e4a86ff7d9bafc5424f2eec6d7262b4
eminarican <emin.arican@outlook.com>
2021-07-30 16:23:13 +0300

    delayed repeating

--- room/scheduler.go
+++ room/scheduler.go
@@ -18,7 +18,7 @@ func (Scheduler) Delayed(delay time.Duration, fun func()) {
 func (Scheduler) Repeating(period time.Duration, fun func(stop func())) func() {
 	ch := make(chan bool)
 	cancel := func() {
-	    ch <- true
+		ch <- true
 	}
 	go func() {
 		ticker := time.NewTicker(period)
@@ -26,7 +26,29 @@ func (Scheduler) Repeating(period time.Duration, fun func(stop func())) func() {
 			select {
 			case <-ticker.C:
 				fun(cancel)
-		    case <-ch:
+			case <-ch:
+				return
+			}
+		}
+	}()
+	return cancel
+}
+
+func (*Scheduler) DelayedRepeating(delay time.Duration, period time.Duration, fun func(stop func())) func() {
+	ch := make(chan bool)
+	cancel := func() {
+		ch <- true
+	}
+	go func() {
+		ticker := time.NewTicker(delay)
+		<-ticker.C
+		ticker.Stop()
+		ticker = time.NewTicker(period)
+		for {
+			select {
+			case <-ticker.C:
+				fun(cancel)
+			case <-ch:
 				return
 			}
 		}
